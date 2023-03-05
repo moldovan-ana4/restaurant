4cd33a0d150e7631e48f6793b192ee0854ecfee8
Ana <mary_anne_007@yahoo.com>
2023-03-05 19:02:34 +0200

    installed font awesome for react

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="icon">
+        <FontAwesomeIcon icon="fa-solid fa-utensils" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
