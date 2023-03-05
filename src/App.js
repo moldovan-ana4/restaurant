cf9b404f87296ccd208c4779a67c72250b8695b3
Ana <mary_anne_007@yahoo.com>
2023-03-05 20:28:12 +0200

    added icons to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-// import Home from "./Components/Home";
-// import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <Routes>{/* <Route path="/" element={<Home />}></Route> */}</Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faMagnifyingGlass,
+  faHeart,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="icon">
-        <FontAwesomeIcon icon="fa-solid fa-utensils" />
+      <div className="">
+        <FontAwesomeIcon id="icons" icon={faUtensils} />
+        <span>resto.</span>
+
+        <FontAwesomeIcon id="icons" icon={faMagnifyingGlass} />
+        <FontAwesomeIcon id="icons" icon={faHeart} />
+        <FontAwesomeIcon id="icons" icon={faCartShopping} />
       </div>
     </div>
   );
