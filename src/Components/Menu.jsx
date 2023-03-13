00568607e83bd065f0dd4cccac482534d71cea55
Ana <mary_anne_007@yahoo.com>
2023-03-13 16:20:33 +0200

    working on menu #0015

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 
+import menu from "../Components/Asests/data/menuItems.json";
+
 const Menu = () => {
-  const [menuList, setMenuList] = useState([]);
-  const [data, setData] = useState([]);
+  const [menuList, setMenuList] = useState(menu);
 
-  
+  // async function getData() {
+  //   console.log("xxxx");
+  //   const result = await fetch("../Components/Asests/data/menuItems.json");
+  //   const res = result.json();
+  //   setData(res);
+  //   console.log(res);
+  // }
 
-  async function getData() {
-    const result = await fetch("./menuItems.json");
-    const res = await result.json();
-    setData(res);
-    console.log(data);
-  }
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
-  useEffect(() => {
-    getData();
-  }, []);
+  console.log(menuList);
 
   return (
     <div>
       <Navbar />
+      <h1>test</h1>
+      {menuList &&
+        menuList.map((item, i) => {
+          return (
+            <div>
+              <h1>{item.name}</h1>
+              <p>{item.ingredients}</p>
+            </div>
+          );
+        })}
     </div>
   );
 };
