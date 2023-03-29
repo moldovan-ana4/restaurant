b786c6f271b39e2fa072b92a887c7a189a95004e
Ana <mary_anne_007@yahoo.com>
2023-03-30 00:22:48 +0300

    #0033

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -3,7 +3,6 @@ import Navbar from "./Navbar";
 import styles from "./Styles/Menu.module.css";
 import { CartContext } from "./Context/Context";
 
-
 export const menuContext = createContext();
 
 const Menu = () => {
@@ -11,51 +10,40 @@ const Menu = () => {
   const [data, setData] = products;
   const [order, setOrder] = orders;
   return (
-      <div>
-        <Navbar />
-        <div className={styles.grid__container}>
-          {data &&
-            data.map((item, i) => {
-              return (
-                <div key={i} className={styles.card}>
-                  <div className={styles.images}>
-                    <img
-                      src={item.picture}
-                      alt="ListMenu"
-                      style={{
-                        aspectRatio: "auto",
-                        width: "350px",
-                        height: "200px",
-                      }}
-                    />
-                    <div className={styles.menu__items}>{item.name}</div>
-                  </div>
-                  <div className={styles.menu__ingredients}>
-                    {item.ingredients}
-                  </div>
-                  <div className={styles.d__flex}>
-                    <div>
-                      <button
-                        onClick={() => {
-                          setOrder((prevState) => [
-                            ...prevState,
-                            {...item,
-                              quantity:1
-                            },
-                          ]);
-                        }}
-                      >
-                        Add To Cart
-                      </button>
-                    </div>
-                    <div className={styles.menu__pricing}>{item.price} RON</div>
-                  </div>
+    <div>
+      <Navbar />
+      <div className={styles.grid__container}>
+        {data &&
+          data.map((item, i) => {
+            return (
+              <div key={i} className={styles.card}>
+                <div className={styles.images}>
+                  <img src={item.picture} alt="ListMenu" />
+                  <div className={styles.menu__items}>{item.name}</div>
                 </div>
-              );
-            })}
-        </div>
+                <div className={styles.menu__ingredients}>
+                  {item.ingredients}
+                </div>
+                <div className={styles.d__flex}>
+                  <div>
+                    <button
+                      onClick={() => {
+                        setOrder((prevState) => [
+                          ...prevState,
+                          { ...item, quantity: 1 },
+                        ]);
+                      }}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                  <div className={styles.menu__pricing}>{item.price} RON</div>
+                </div>
+              </div>
+            );
+          })}
       </div>
-    // </menuContext.Provider>
+    </div>
   );
 };
 export default Menu;
--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -21,15 +21,15 @@ const Review = () => {
       <Navbar />
       <h3>What our customers say about the menu?</h3>
 
-      <div className={styles.review__container}>
+      <div >
         {reviews.map((item, i) => {
           return (
-            <div key={i}>
+            <div className={styles.review__container} key={i}>
               <div className={styles.review__img}>
                 <img src={item.image} alt={item.image} />
-              </div>
               <div className={styles.review__name}>
                 <span>{item.name}</span>
+              </div>
               </div>
               <div className={styles.review__message}>
                 <p>{item.message}</p>
