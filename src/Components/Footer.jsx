1f76237f878484c3d09a0fe94aecb251c4524f3a
Ana <mary_anne_007@yahoo.com>
2023-04-01 17:41:08 +0300

    #0039

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
+import styles from "./Styles/Footer.module.css";
 
 const Footer = () => {
   return (
     <div>
-      <p>Footer</p>
-    </div>
-  )
-}
+      <div className={styles.footer__container}>
+        <div className={styles.footer__logo}>
+          <span>
+            <FontAwesomeIcon icon={faUtensils} />
+          </span>
+          <span> Resto. </span>
+        </div>
 
-export default Footer
+        <div className={styles.footer__adress}>
+          ADRESS:
+          <p>Street Deva 46, Cluj-Napoca 400535</p>
+          <p>Phone Number: 0743 911 520</p>
+          <p>Email: contact@restopizza.ro</p>
+        </div>
+        <div className={styles.footer__follow__us}>
+          FOLLOW US:
+          <div>
+            <span className={styles.footer__icons}>{<AiFillInstagram />}</span>
+            <span> Instagram</span>
+          </div>
+          <div>
+            <span className={styles.footer__icons}>{<AiFillFacebook />}</span>
+            <span> Facebook</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
