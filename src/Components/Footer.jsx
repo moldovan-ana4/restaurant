dd2cd2803430629da68f4cd9cbf88063566de085
Ana <mary_anne_007@yahoo.com>
2023-04-26 22:11:01 +0300

    footer links work

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUtensils } from "@fortawesome/free-solid-svg-icons";
@@ -6,41 +7,45 @@ import styles from "./Styles/Footer.module.css";
 
 const Footer = () => {
   return (
-      <footer className={styles.footer__container}>
-        <div className={styles.footer__logo}>
-          <span className={styles.footer__icons}>
-            {" "}
-            <FontAwesomeIcon icon={faUtensils} />
+    <footer className={styles.footer__container}>
+      <div className={styles.footer__logo}>
+        <span className={styles.footer__icons}>
+          {" "}
+          <FontAwesomeIcon icon={faUtensils} />
+        </span>
+        <span> Resto.</span>
+      </div>
+
+      <div className={styles.footer__follow__us}>
+        <h4>Follow us: </h4>
+        <div className={styles.footer__socials}>
+          <span className={styles.footer__icons}>{<AiFillInstagram />}</span>
+          <span>
+            <Link to="/">Instagram</Link>
           </span>
-          <span> Resto.</span>
         </div>
 
-        <div className={styles.footer__follow__us}>
-          <h4>Follow us: </h4>
-          <div className={styles.footer__socials}>
-            <span className={styles.footer__icons}>{<AiFillInstagram />}</span>
-            <span> Instagram</span>
-          </div>
-
-          <div className={styles.footer__socials}>
-            <span className={styles.footer__icons}>{<AiFillFacebook />}</span>
-            <span> Facebook</span>
-          </div>
+        <div className={styles.footer__socials}>
+          <span className={styles.footer__icons}>{<AiFillFacebook />}</span>
+          <span>
+            <Link to="/">Facebook</Link>
+          </span>
         </div>
+      </div>
 
-        <div className={styles.footer__adress}>
-          <h4>Contact: </h4>
-          <ul> </ul>
-          <li>Phone Number: 0743 911 520</li>
-          <li>Email: contact@resto.ro</li>
-          <li>Street Deva 46, Cluj-Napoca</li>
-        </div>
+      <div className={styles.footer__adress}>
+        <h4>Contact: </h4>
+        <ul> </ul>
+        <li>Phone Number: 0743 911 520</li>
+        <li>Email: contact@resto.ro</li>
+        <li>Street Deva 46, Cluj-Napoca</li>
+      </div>
 
-          <hr />
-        <div className={styles.footer__copyright}>
-          Copyright 2020 Resto. | All Rights Reserved
-        </div>
-      </footer>
+      <hr />
+      <div className={styles.footer__copyright}>
+        Copyright 2020 Resto. | All Rights Reserved
+      </div>
+    </footer>
   );
 };
 
